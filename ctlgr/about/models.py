db76a21bcd2ae066ef0b09022994b6834c5ef9a2
Mario Felipe Rinaldi <mrinaldi@sp.r7.com>
2011-04-26 15:19:56 -0300

    about: new way of make
    -> generics
    -> english models.py

--- ctlgr/about/models.py
+++ ctlgr/about/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Sobre(models.Model):
+class About(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     text = models.TextField()
--- ctlgr/about/views.py
+++ ctlgr/about/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response, get_list_or_404
 from django.template import Context, loader
-from ctlgr.about.models import Sobre
+from ctlgr.about.models import About
 
 def index(request):
-  latest_list = Sobre.objects.all().order_by('id')[:5]
+  items = About.objects.all().order_by('id')[:5]
   context = Context({
-    'latest_list': latest_list,
+    'items': items,
   })
   return render_to_response('about/index.html', context)
 
